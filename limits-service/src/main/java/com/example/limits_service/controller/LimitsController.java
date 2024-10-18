76838876088230f0a45ec28169e0969f1403633f
haipham2004 <bophamnb2004@gmail.com>
2024-10-18 11:08:18 +0700

    TASC

---
+++ limits-service/src/main/java/com/example/limits_service/controller/LimitsController.java
@@ -0,0 +1,22 @@
+package com.example.limits_service.controller;
+
+import com.example.limits_service.bean.Limits;
+import com.example.limits_service.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(configuration.getMinimum(),
+                configuration.getMaximum());
+    }
+}
